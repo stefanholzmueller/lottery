a02a2784a130292f6b7e3deeaeebfbd24adba76b
Stefan Holzmüller <stefanholzmueller@github>
2013-12-07 16:36:02 +0100

    initial setup

---
+++ lottery.js
@@ -0,0 +1,7 @@
+/// <reference path="lib/angular.d.ts" />
+/// <reference path="lib/underscore.d.ts" />
+var app = angular.module("lottery", []);
+
+app.controller("LotteryController", function ($scope) {
+});
+//# sourceMappingURL=lottery.js.map
---
+++ lottery.ts
@@ -0,0 +1,8 @@
+/// <reference path="lib/angular.d.ts" />
+/// <reference path="lib/underscore.d.ts" />
+
+var app = angular.module("lottery", []);
+
+app.controller("LotteryController", function ($scope) {
+
+});
