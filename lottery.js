133ae3ffc7fb6c72023f356f8a5d2772f6d14f0b
Stefan Holzmüller <stefanholzmueller@github>
2013-12-08 12:20:40 +0100

    added a list of participants and a form to add new ones

--- lottery.js
+++ lottery.js
@@ -3,5 +3,14 @@
 var app = angular.module("lottery", []);
 
 app.controller("LotteryController", function ($scope) {
+    $scope.participants = [
+        { name: "Tester", active: true }
+    ];
+
+    $scope.addParticipant = function () {
+        var newName = $scope.newParticipant;
+        $scope.participants.push({ name: newName, active: true });
+        $scope.newParticipant = "";
+    };
 });
 //# sourceMappingURL=lottery.js.map
