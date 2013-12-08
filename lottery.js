9069f2516c954bb91ea12edc21cf2d32ec9f1aec
Stefan Holzmüller <stefanholzmueller@github>
2013-12-08 13:22:46 +0100

    implemented drawing a winner

--- lottery.js
+++ lottery.js
@@ -1,16 +1,50 @@
-/// <reference path="lib/angular.d.ts" />
-/// <reference path="lib/underscore.d.ts" />
+/// <reference path="lib/angular/angular.d.ts" />
+/// <reference path="lib/lodash/lodash.d.ts" />
 var app = angular.module("lottery", []);
 
-app.controller("LotteryController", function ($scope) {
-    $scope.participants = [
-        { name: "Tester", active: true }
-    ];
+app.controller("LotteryController", [
+    '$scope',
+    'LotteryService',
+    function ($scope, service) {
+        $scope.closed = false;
+        $scope.participants = [
+            { name: "Tester", active: true }
+        ];
 
-    $scope.addParticipant = function () {
-        var newName = $scope.newParticipant;
-        $scope.participants.push({ name: newName, active: true });
-        $scope.newParticipant = "";
+        $scope.addParticipant = function () {
+            var newName = $scope.newParticipant;
+            var exists = _.find($scope.participants, function (x) {
+                return x.name === newName;
+            });
+            if (exists) {
+                alert("Name schon in Liste vorhanden");
+            } else {
+                $scope.participants.push({ name: newName, active: true });
+                $scope.newParticipant = "";
+            }
+        };
+
+        $scope.draw = function () {
+            var chosen = _.filter($scope.participants, function (x) {
+                return x.active;
+            });
+            var names = _.map(chosen, function (x) {
+                return x.name;
+            });
+            if (names.length > 0) {
+                $scope.closed = true;
+                $scope.winner = service.draw(names);
+            }
+        };
+    }
+]);
+
+app.service("LotteryService", function () {
+    return {
+        draw: function (names) {
+            var randomized = _.shuffle(names);
+            return _.first(randomized);
+        }
     };
 });
 //# sourceMappingURL=lottery.js.map
--- lottery.ts
+++ lottery.ts
@@ -3,14 +3,38 @@
 
 var app = angular.module("lottery", []);
 
-app.controller("LotteryController", function ($scope) {
+app.controller("LotteryController", ['$scope', 'LotteryService', function ($scope, service) {
+	$scope.closed = false;
 	$scope.participants = [
 		{name: "Tester", active: true}
 	];
 
 	$scope.addParticipant = function () {
 		var newName = $scope.newParticipant;
-		$scope.participants.push({name: newName, active: true});
-		$scope.newParticipant = "";
+		var exists = _.find($scope.participants, x => x.name === newName)
+		if (exists) {
+			alert("Name schon in Liste vorhanden")
+		} else {
+			$scope.participants.push({name: newName, active: true});
+			$scope.newParticipant = "";
+		}
+	}
+
+	$scope.draw = function () {
+		var chosen = _.filter($scope.participants, x => x.active);
+		var names = _.map(chosen, x => x.name);
+		if (names.length > 0) {
+			$scope.closed = true;
+			$scope.winner = service.draw(names);
+		}
+	}
+}]);
+
+app.service("LotteryService", function () {
+	return {
+		draw: function (names) {
+			var randomized = _.shuffle(names);
+			return _.first(randomized);
+		}
 	}
 });
