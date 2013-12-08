52be73ef3a82ef53072908bd9b0eeac07ef73bab
Stefan Holzmüller <stefanholzmueller@github>
2013-12-08 13:10:37 +0100

    upgraded and restructured libs

--- lottery.ts
+++ lottery.ts
@@ -1,5 +1,5 @@
-/// <reference path="lib/angular.d.ts" />
-/// <reference path="lib/underscore.d.ts" />
+/// <reference path="lib/angular/angular.d.ts" />
+/// <reference path="lib/lodash/lodash.d.ts" />
 
 var app = angular.module("lottery", []);
 
